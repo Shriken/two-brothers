d42baa5a8cfe450f1ebe18cae4051c85d791af09
Zane Sterling <sterling.zane@gmail.com>
2016-12-07 02:54:12 -0500

    Write infrastructure for graph generation types

--- source/state/sim_state.d
+++ source/state/sim_state.d
@@ -3,6 +3,11 @@ module state.sim_state;
 import graph;
 import misc.utils;
 
+enum GenerationTechnique {
+	ring,
+	smart_random,
+}
+
 class SimulationState {
 	static const int NUM_NODES = 10;
 
@@ -12,7 +17,9 @@ class SimulationState {
 	Graph graph;
 	Node curNode;
 
-	this() {
+	GenerationTechnique generation_technique;
+
+	this(GenerationTechnique gen_technique=GenerationTechnique.ring) {
 		graph = generateGraph(NUM_NODES);
 		curNode = graph.nodes[0];
 		import std.stdio;
@@ -20,13 +27,27 @@ class SimulationState {
 	}
 
 	Graph generateGraph(int size) {
+		switch (generation_technique) {
+			case GenerationTechnique.ring:
+				return generateGraphRing(size);
+			case GenerationTechnique.smart_random:
+				return generateGraphSmartRandom(size);
+			default:
+				return null;
+		}
+	}
+
+	Graph generateGraphRing(int size) {
 		Graph g = new Graph(size);
 		foreach (i; 0 .. size) {
 			g.nodes[i].setLeft( g.nodes[(i - 1).remainder(size)]);
 			g.nodes[i].setRight(g.nodes[(i + 3).remainder(size)]);
 		}
-
 		return g;
+	}
+
+	Graph generateGraphSmartRandom(int size) {
+		return null;
 	}
 
 	void update() {}
